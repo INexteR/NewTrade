44995e7eba1d16510522b73a7813e6ac2a8d7920
EldHasp <EldHasp@Gmail.com>
2023-02-27 21:29:04 +0300

    Валидация ошибок после первоого ввода в поле.

--- NewTradeSln/NewTrade/Views/LoginView.xaml.cs
+++ NewTradeSln/NewTrade/Views/LoginView.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace NewTrade.Views
+﻿using ShopViewModels;
+using System.Windows.Data;
+
+namespace NewTrade.Views
 {
     /// <summary>
     /// Логика взаимодействия для LoginView.xaml
@@ -8,6 +11,29 @@
         public LoginView()
         {
             InitializeComponent();
+        }
+
+        private static readonly Binding loginBinding = new Binding(nameof(LoginPassword.Login))
+        {
+            UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+        };
+        private static readonly Binding passwordBinding = new Binding(nameof(LoginPassword.Password))
+        {
+            UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+        };
+
+        private void OnEnabledValidation(object sender, TextChangedEventArgs e)
+        {
+            if (sender == loginTBox)
+            {
+                loginTBox.SetBinding(TextBox.TextProperty, loginBinding);
+                loginTBox.TextChanged -= OnEnabledValidation;
+            }
+            else if (sender == passwordTBox)
+            {
+                passwordTBox.SetBinding(TextBox.TextProperty, passwordBinding);
+                passwordTBox.TextChanged -= OnEnabledValidation;
+            }
         }
     }
 }
