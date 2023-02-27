f9cfa6396a29fa33543b1171889dd87e9c9c4ef9
EldHasp <EldHasp@Gmail.com>
2023-02-27 14:27:51 +0300

    Изменена валидация на странице авторизации.

--- NewTradeSln/NewTrade/Views/LoginView.xaml.cs
+++ NewTradeSln/NewTrade/Views/LoginView.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace NewTrade.Views
+﻿using ShopViewModels;
+using System.Windows.Data;
+
+namespace NewTrade.Views
 {
     /// <summary>
     /// Логика взаимодействия для LoginView.xaml
@@ -8,6 +11,20 @@
         public LoginView()
         {
             InitializeComponent();
+        }
+
+        private static readonly Binding loginBinding = new Binding(nameof(LoginPassword.Login))
+        {
+            UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+        };
+        private static readonly Binding passwordBinding = new Binding(nameof(LoginPassword.Password))
+        {
+            UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+        };
+        private void OnAuthorize(object sender, RoutedEventArgs e)
+        {
+            loginTBox.SetBinding(TextBox.TextProperty, loginBinding);
+            passwordTBox.SetBinding (TextBox.TextProperty, passwordBinding);
         }
     }
 
