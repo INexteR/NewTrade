f493f76d11cd17c11e8f36bcd8dbbb0bbd2bea47
EldHasp <EldHasp@Gmail.com>
2023-03-01 09:37:30 +0300

    Изменил интерфейсы источников товаров, и производителей.

--- NewTradeSln/CommonNet6/Model/IManufacturersSource.cs
+++ NewTradeSln/CommonNet6/Model/IManufacturersSource.cs
@@ -1,11 +1,26 @@
 ﻿using CommonNet6.Collection;
+using System;
 using System.Collections.Generic;
 
 namespace Model
 {
+    /// <summary>Поставщик (источник) производителей.</summary>
     public interface IManufacturersSource
     {
+        // TODO: Надо выбрать какое из этих двух событий оставить.
+
+        /// <summary>Такое событие говорит нам, что список производителей не предназначен
+        /// для изменения в потребителях Модели, но может меняться в Бизнес Логике Модели.</summary>
         event NotifyListChangedEventHandler<IManufacturer> ManufacturerChanged;
-        IReadOnlyCollection<IManufacturer> GetManufacturers();
+
+        /// <summary>Такое событие говорит нам, что список производителей не предназначен
+        /// для изменения в потребителях Модели и может меняться в Бизнес Логике Модели
+        /// только целиком весь разом.</summary>
+        event EventHandler ManufacturersChanged;
+
+        /// <summary>Возвращает всех производителей в индексированном списке
+        /// несвязанном с Бизнес (Доменной) логикой.</summary>
+        /// <returns>Индексированная коллекция (лист, массив и др.) всех производителей.</returns>
+        IReadOnlyList<IManufacturer> GetManufacturers();
     }
 }
--- NewTradeSln/CommonNet6/Model/IProductsSource.cs
+++ NewTradeSln/CommonNet6/Model/IProductsSource.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 
 namespace Model
 {
+    /// <summary>Поставщик (источник) товаров.</summary>
     public interface IProductsSource
     {
+        /// <summary>Извещает об изменение в списке товаров: полной его смене,
+        /// удалении-добавлении товара, обновлении товара.</summary>
         event NotifyListChangedEventHandler<IProduct> ProductChanged;
-        IList<IProduct> GetProducts();
 
+        /// <summary>Возвращает все товары в индексированном списке
+        /// несвязанном с Бизнес (Доменной) логикой.</summary>
+        /// <returns>Индексированная коллекция (лист, массив и др.) всех товаров.</returns>
+        IReadOnlyCollection<IProduct> GetProducts();
+
+        /// <summary>Добавление товара.</summary>
+        /// <param name="product">Добавляемый товар.</param>
         void Add(IProduct product);
+
+        /// <summary>Удаление товара.</summary>
+        /// <param name="product">Удаляемый товар.</param>
         void Remove(IProduct product);
+
+        /// <summary>Изменение товара.</summary>
+        /// <param name="product">Изменяемый товар.</param>
         void Change(string ArticleNumber, IProduct product);
+
+        /// <summary>Переинициализация списка товаров из Хранилища Данных.</summary>
         void Refresh();
     }
 }
