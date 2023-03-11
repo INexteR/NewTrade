e73948038cb78776f1e0c387eb21888c2dc16833
EldHasp <eldhasp@gmail.com>
2023-03-11 19:16:44 +0300

    Добавлен Not конвертер.

---
+++ NewTradeSln/NewTrade/Converters/BooleanNotConverterExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace NewTrade.Converters
+{
+    /// <summary>Предоставляет экземпляр <see cref="BooleanNotConverter"/> из <see cref="BooleanNotConverter.Instance"/>.</summary>
+    [MarkupExtensionReturnType(typeof(BooleanNotConverter))]
+    public class BooleanNotExtension : MarkupExtension
+    {
+        /// <summary>Возвращает экземпляр конвертера из <see cref="BooleanNotConverter.Instance"/>.</summary>
+        /// <param name="serviceProvider">Вспомогательный объект поставщика служб,
+        /// способный предоставлять службы для расширения разметки.<para/>
+        /// Не используется.</param>
+        /// <returns>Экземпляр из <see cref="BooleanNotConverter.Instance"/>.</returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+            => BooleanNotConverter.Instance;
+
+        /// <summary>Создаёт экземпляр <see cref="BooleanNotExtension"/>.</summary>
+        public BooleanNotExtension() { }
+    }
+}
