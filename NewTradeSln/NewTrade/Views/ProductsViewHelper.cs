619d5b36612e67a5855ac9b3fe0eb940449f0fbc
EldHasp <eldhasp@gmail.com>
2023-03-20 22:26:35 +0300

    Пустое контекстное меню не показывается.

--- NewTradeSln/NewTrade/Views/ProductsViewHelper.cs
+++ NewTradeSln/NewTrade/Views/ProductsViewHelper.cs
@@ -2,6 +2,14 @@
 using System;
 using ViewModels;
 using System.IO;
+using System.Windows.Input;
+using ShopViewModels;
+using System.Windows.Data;
+using System.Globalization;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Runtime.Intrinsics.X86;
+using System.Media;
 
 namespace NewTrade.Views
 {
@@ -48,5 +56,59 @@ namespace NewTrade.Views
 
             return (viewModel, product);
         }
+
+        /// <summary>Какое нужно действие - передаётся через параметр команды.</summary>
+        public static RoutedUICommand General { get; } = new RoutedUICommand("Общая команда", nameof(General), typeof(ProductsViewHelper));
+
+        public static ExecutedRoutedEventHandler Executed { get; } = (s, e) =>
+        {
+            FrameworkElement view = (FrameworkElement)s;
+            IProductsViewModel viewModel = (IProductsViewModel)view.DataContext;
+            FrameworkElement source = (FrameworkElement)e.OriginalSource;
+            IProduct? product = source.DataContext as IProduct;
+            string action = e.Parameter?.ToString() ?? string.Empty;
+
+            MessageBox.Show($"view = \"{view}\"\r\nviewModel = \"{viewModel}\r\nsource = \"{source}\"\r\nтовар = \"{product?.Name}\"\r\nдействие = \"{action}");
+        };
+
+        private static readonly TempProduct temp = new();
+
+        public static CanExecuteRoutedEventHandler CanExecute { get; } = (s, e) =>
+        {
+            FrameworkElement view = (FrameworkElement)s;
+            IProductsViewModel viewModel = (IProductsViewModel)view.DataContext;
+            FrameworkElement source = (FrameworkElement)e.OriginalSource;
+            IProduct? product = source.DataContext as IProduct;
+            string commandName = e.Parameter?.ToString() ?? string.Empty;
+
+            if (viewModel is null)
+            {
+                return;
+            }
+
+            e.CanExecute = IsAccessibleCommand(viewModel, commandName);
+        };
+
+        public static bool IsAccessibleCommand(IProductsViewModel viewModel, string commandName)
+        {
+            return commandName?.ToUpper() switch
+            {
+                "ADD" => viewModel.AddProduct.CanExecute(temp),
+                "REMOVE" => viewModel.RemoveProduct.CanExecute(temp),
+                "UPDATE" => viewModel.UpdateProduct.CanExecute(temp),
+                _ => false
+            };
+
+        }
+
+        public static RoutedEventHandler ContextMenuOpened { get; } = (s, _) =>
+        {
+            ContextMenu contextMenu = (ContextMenu)s;
+            if(contextMenu.ActualHeight == 0)
+            {
+                contextMenu.IsOpen = false;
+                SystemSounds.Beep.Play();
+            }
+        };
     }
 }
